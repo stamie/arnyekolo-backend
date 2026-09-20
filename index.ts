314943f94a311813c3c3f420b66cdd5034fa0581
stamie <estampel@gmail.com>
2026-09-20 18:24:17 +0200

    modified project  post

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 // Teszt végpont
