8f3e8bdd810365dfffeec9b0d9582bd63b685279
stamie <estampel@gmail.com>
2026-09-20 20:00:44 +0200

    modified project  post

--- index.ts
+++ index.ts
@@ -24,7 +24,7 @@ async function sendCalculation() {
     try {
       const response = await collection.insertOne({insert: "ping"});
     } catch (error) {
-    console.error("Error occurred while inserting document:", error);
+      return res.status(500).json({ error: "Error occurred while inserting document:", error });
     }
   } finally {
     // Ensures that the client will close when you finish/error
