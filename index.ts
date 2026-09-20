e7b29c071ea4b6d3957d46976177145b84371a88
stamie <estampel@gmail.com>
2026-09-20 19:11:50 +0200

    modified project  post

--- index.ts
+++ index.ts
@@ -18,7 +18,7 @@ async function sendCalculation() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("orders").collection("orders").insertOne({"insert": "ping"});
+    await client.db("orders").collection("orders").insertOne({insert: "ping"});
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
