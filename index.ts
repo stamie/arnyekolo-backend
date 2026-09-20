99809d8b4033c22f8d58e094e7e4871c0acf6806
stamie <estampel@gmail.com>
2026-09-20 18:57:59 +0200

    modified project  post

--- index.ts
+++ index.ts
@@ -1,6 +1,31 @@
 import express from 'express';
 import cors from 'cors';
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://estampel_db_user:AxoQHPVCm3SFIwSD@arnyekolo.kwdrczy.mongodb.net/?appName=arnyekolo";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function sendCalculation() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").insertOne("orders", {"insert": "ping"});
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -50,6 +75,18 @@ app.post('/backend', (req, res) => {
   if (!req.body.user || !req.body.password) {
     return res.status(400).json({ error: 'Hiányzó paraméterek. Kérlek add meg a user és password paramétereket.' });
   }
+  const user = req.body.user;
+  const password = req.body.password;
+
+  // Itt végezheted el a backend logikát, például ellenőrizheted a felhasználót és jelszót.
+  // Például:
+  if (user === 'admin' && password === 'password123') {
+    mongoose.connect('mongodb://localhost:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true });
+    return res.json({ message: 'Sikeres bejelentkezés!' });
+  } else {
+    return res.status(401).json({ error: 'Hibás felhasználónév vagy jelszó.' });
+  }
+  
 });
 // A szerver elin1dítása (FONTOS: process.env.PORT kell a  Render-nek!)
 app.listen(PORT, () => {
